6e557805cf1938138ae5252733dbe03a9dd135f3
K <Kai@Kais-MacBook-Air.local>
2016-07-27 09:32:03 -0700

    updated code from working on MacMini
    7/27/16 @ 9:31AM

---
+++ FI2/RotationDictionary.swift
@@ -0,0 +1,13 @@
+//
+//  RotationDictionary.swift
+//  FI3
+//
+//  Created by Kai Drayton-Yee on 7/26/16.
+//  Copyright © 2016 Kai Drayton-Yee. All rights reserved.
+//
+
+import Foundation
+
+class RotationDictionary{
+	static var tDegrees : [String: Int] = ["blackRingSetOne": 0,"blackRingSetTwo": 0, "blackRingSetThree": 0, "blackRingSetFour": 0, "blackRingSetFive": 0]
+}
